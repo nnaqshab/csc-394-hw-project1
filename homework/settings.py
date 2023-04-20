c517f4df9e022d95471c4d8d0ec11ab37d8f750b
Noor Naqshab's <nnaqshab@depaul.edu>
2023-04-20 08:45:31 +0500

    .

--- homework/settings.py
+++ homework/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-z0u*c#$1%#m*785&8ohhla!5ce4d2a$$-i$+-k#+u%*gfy4-_h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS=['*']
 
 
 # Application definition
@@ -73,10 +74,20 @@ WSGI_APPLICATION = 'homework.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'naqshab',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
