c6c7de662900ff29207e844a4fd8724ad6d0f2bd
Noor Naqshab's <nnaqshab@depaul.edu>
2023-04-20 09:16:30 +0500

    Lab 8

--- homework/settings.py
+++ homework/settings.py
@@ -31,6 +31,14 @@ ALLOWED_HOSTS=['*']
 
 # Application definition
 
+# INSTALLED_APPS = [
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
+# ]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'students',
 ]
 
 MIDDLEWARE = [
